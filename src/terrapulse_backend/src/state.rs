121439ee1248628c8d244df11638310fe6b7f7aa
H3nryK <henrykimani89@gmail.com>
2024-11-22 21:43:39 +0300

    start all a afresh

--- src/terrapulse_backend/src/state.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) use std::cell::RefCell;
-use std::collections::HashMap;
-
-thread_local! {
-    static USERS: RefCell<HashMap<Principal, User>> = RefCell::new(HashMap::new());
-    static NFTS: RefCell<HashMap<String, NFT>> = RefCell::new(HashMap::new());
-    static MARKETPLACE_LISTINGS: RefCell<HashMap<String, u64>> = RefCell::new(HashMap::new());
-    static CONSERVATION_PROJECTS: RefCell<HashMap<String, ConservationProject>> = RefCell::new(HashMap::new());
-}
